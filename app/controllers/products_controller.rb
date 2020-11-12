c32523b82172226585fda16814ab09b349dea243
shintarou-satou <shintarou.kh@gmail.com>
2020-11-13 01:55:51 +0900

    商品表示機能の実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,7 @@ class ProductsController < ApplicationController
   before_action :authenticate_user!, except:[:index]
 
   def index
-    # @products = Product.order("created_at DESC")
+    @products = Product.order("created_at DESC")
   end
 
   def new
@@ -28,7 +28,7 @@ private
 
   def  product_params
     params.require(:product).permit(
-      :imege, :product, :explanation, :category_id, :condition_id, :delivery_fee_id, :todoufuken_address_id, :shipping_time_id, :price).
+      :image, :product, :explanation, :category_id, :condition_id, :delivery_fee_id, :todoufuken_address_id, :shipping_time_id, :price).
       merge(user_id: current_user.id)
 
   end
