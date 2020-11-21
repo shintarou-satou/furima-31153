6b98c93f565c8dd607b00f388ede2fc8a7a0afde
shintarou-satou <shintarou.kh@gmail.com>
2020-11-21 15:46:24 +0900

    Revert "デプロイ後、修正あり"
    This reverts commit 9a5aed22bcc934ea57b0d20ec29875f818223397.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, except:[:index, :show]
   before_action :set_product, only: [:show, :edit, :update, :destroy]
-  before_action :move_to_index, except:[:index, :show, :new, :create,]
+  before_action :move_to_index, except:[:index, :show, :new, :create]
 
   def index
     @products = Product.order("created_at DESC")
@@ -25,9 +25,6 @@ class ProductsController < ApplicationController
   end
 
   def edit
-      if @product.order != nil
-        redirect_to root_path
-      end
   end
 
   def update
@@ -58,7 +55,7 @@ class ProductsController < ApplicationController
   end
 
   def move_to_index
-      unless current_user.id  == @product.user.id 
+      unless current_user.id  == @product.user.id
         redirect_to  root_path
       end
   end
